c033bee1b440a48c91ec4ca0b56246a585c17fc2
Cameron Paczek <cpazek@gmail.com>
2022-09-12 13:05:28 -0700

    changes

--- index.test.js
+++ index.test.js
@@ -1,2 +1,14 @@
-
+const verifyEmail = require('./index')
+describe("Test email verification function", () =>{
+    it('should return true if email is valid', ()=>{
+        const email = 'test@google.com'
+        const result = verifyEmail(email)
+        expect(result).toBe(true)
+    })
+    it('should return false if email is invalid', ()=>{
+        const email = 'foobar'
+        const result = verifyEmail(email)
+        expect(result).toBe(false)
+    })
+})
 
